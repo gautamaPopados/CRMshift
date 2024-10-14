50e1db48455bfc3bcfd596161289683a4f6b8ff7
gautamaPopados <155830875+gautamaPopados@users.noreply.github.com>
2024-10-14 16:51:20 +0700

    initial commit

---
+++ src/main/java/com/gautama/crmshift/CrMshiftApplication.java
@@ -0,0 +1,13 @@
+package com.gautama.crmshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrMshiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrMshiftApplication.class, args);
+    }
+
+}
